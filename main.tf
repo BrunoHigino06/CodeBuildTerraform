cad0764a990970885fb6aca9f7fcdfa5be0d3dda
Bruno Higino de Souza <brunohigino06@gmail.com>
2021-12-22 13:02:49 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,6 +57,19 @@ resource "aws_security_group_rule" "FrontEndSGIngressSSH" {
   ]
 }
 
+resource "aws_security_group_rule" "FrontEndSGIngressSSH" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.FrontEndSG.id
+
+  depends_on = [
+    aws_security_group.FrontEndSG
+  ]
+}
+
 #Egress
 
 resource "aws_security_group_rule" "FrontEndSGEgress" {
